c28da101b2561050ecdf1bdf98c69fac10a321a6
Oláh Bendegúz István <obendi2003@gmail.com>
2024-09-10 18:31:47 +0200

    initial state of the django server done

---
+++ backend/movie_recommender/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    director = models.CharField(max_length=100)
+    year = models.IntegerField()
+    rating = models.FloatField()
+    description = models.TextField()
+    #image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
