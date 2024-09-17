e835afef439dcbe79efaeaae36d2cc4d49821370
Oláh Bendegúz István <obendi2003@gmail.com>
2024-09-17 18:39:08 +0200

    change into flask

--- backend/movie_recommender/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Movie
-# Register your models here.
-class MovieAdmin(admin.ModelAdmin):
-    list_display = ('id','title', 'genre', 'director', 'year', 'rating','description')
-    
-admin.site.register(Movie, MovieAdmin)  
--- backend/movie_recommender/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .serializers import MovieSerializer
-from .models import Movie
-# Create your views here.
-class MovieView(viewsets.ModelViewSet):
-    serializer_class = MovieSerializer
-    queryset = Movie.objects.all()
